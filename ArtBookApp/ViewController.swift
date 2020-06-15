1b8f4b6cab218e7e9a9825a1ca03296be41545a2
Emil Vaklinov <emil_vaklinov@yahoo.com>
2020-06-15 13:00:52 +0100

    Initial Commit

---
+++ ArtBookApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtBookApp
+//
+//  Created by Emil Vaklinov on 15/06/2020.
+//  Copyright © 2020 Emil Vaklinov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
